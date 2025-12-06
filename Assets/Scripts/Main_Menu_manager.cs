d186adeaaa2c19847c3e6f0b0d5f5d20ff245753
Thomas Nikolopoylos <tommot38@gmai.com>
2025-12-06 22:22:02 +0200

    ..

--- Assets/Scripts/Main_Menu_manager.cs
+++ Assets/Scripts/Main_Menu_manager.cs
@@ -65,7 +65,7 @@ public class Main_Menu_manager : MonoBehaviour
             if (i + 1 > level_reached)    // lock next levels
             {
                 LevelButtons[i].interactable = false;
-                LevelButtons[i].GetComponent<ImageBlurrer>().ToggleBlur(true);
+                LevelButtons[i].GetComponent<ImageBlurrer>().ToggleBlur(true);      //blur locked level
             }
         }
     }
